1e911ac8389d0dc5ae74c73eb8874bd237de86c9
Alice Daubree <alice.daubree@yahoo.fr>
2022-09-02 16:51:04 +0200

    mef comp

--- app/javascript/controllers/display_result_controller.js
+++ app/javascript/controllers/display_result_controller.js
@@ -4,7 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form", "caroussel"]
   connect() {
-    console.log("test15")
   }
 
   display(event) {
