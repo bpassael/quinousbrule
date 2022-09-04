a402b948852a62efa991901833ebb0429916a703
Bastien Perez Assael <perez.bas@gmail.com>
2022-09-04 11:29:32 +0200

    recherche députés

--- app/javascript/controllers/display_result_controller.js
+++ app/javascript/controllers/display_result_controller.js
@@ -1,10 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
+import {timeConvert} from './utilities.js';
 
 // Connects to data-controller="display-result"
 export default class extends Controller {
   static targets = ["form", "caroussel", "comparated", "annualfp", "jetmn", "kmyacht", "tweet"]
   connect() {
+    console.log("test5")
   }
+
 
   display(event) {
     event.preventDefault()
@@ -20,17 +23,24 @@ export default class extends Controller {
         let result = data.match(/"resultvalue">([\d\.]+)/)
         result = result[1]
         console.log(result)
-        const annualFootprint = Number.parseFloat(result).toFixed(2)
-        const jetComparaison = (Number.parseInt(result)/78).toFixed(2)
-        const yachtComparaison = (Number.parseInt(result)/230).toFixed(2)
+        const annualFootprint = Math.round(Number.parseFloat(result))
+        const jetComparaison = timeConvert((Number.parseInt(result)/78))
+        const yachtComparaison = Math.round(Number.parseInt(result)/230)
         //const resultContent = `Votre empreinte carbone annuelle est de ${result} kgCCO2e. Cela correspond à la consommation en carburant de ${Number.parseInt(result)/78} minutes de vol du jet privé de Vincent Bolloré, ou de ${Number.parseInt(result)/230} kilomètres parcourus par le yacht de Bernard Arnault.`
         this.carousselTarget.classList.toggle('d-none')
         this.comparatedTarget.classList.toggle('d-none')
         this.annualfpTarget.innerText = annualFootprint
         this.jetmnTarget.innerText = jetComparaison
         this.kmyachtTarget.innerText = yachtComparaison
-        this.tweetTarget.href = `https://twitter.com/intent/tweet?text=Mon empreinte CO2 annuelle est de ${annualFootprint} kgCO2e. Cela correspond à la consommation carburant de ${jetComparaison} minutes de vol du jet privé de Vincent Bolloré, ou de ${yachtComparaison} kilomètres parcourus par le yacht de Bernard Arnault. Que font nos représentants ? &hashtags=quinousbrule`
+        this.tweetTarget.href = `https://twitter.com/intent/tweet?text=Mon empreinte CO2 annuelle est de ${annualFootprint} kgCO2e. Cela correspond à la consommation carburant de ${jetComparaison} de vol du jet privé de Vincent Bolloré, ou de ${yachtComparaison} kilomètres parcourus par le yacht de Bernard Arnault. Que font nos représentants ? &hashtags=quinousbrule`
 
       })
   }
+
+
+
+
+
+
+
 }
---
+++ app/javascript/controllers/utilities.js
@@ -0,0 +1,70 @@
+export function timeConvert(n) {
+  var num = n;
+  if (num < 10) {
+    return num.toFixed(2) + " minutes"
+  }
+  var hours = (num / 60);
+  var rhours = Math.floor(hours);
+  var minutes = (hours - rhours) * 60;
+  var rminutes = Math.ceil(minutes);
+  if (rhours === 0 && rminutes === 1) {
+    return rminutes + " minute";
+  } else if (rhours === 0) {
+    return rminutes + " minutes";
+  } else if (rhours === 1 && rminutes === 0) {
+    return rhours + " heure";
+  } else if (rminutes === 0) {
+      return rhours + " heures";
+  } else if (rhours === 1 && rminutes === 1) {
+    return rhours + " heure et " + rminutes + " minute";
+  } else if (rhours === 1) {
+    return rhours + " heure et " + rminutes + " minutes";
+  } else  if (rminutes === 1) {
+    return rhours + " heures et " + rminutes + " minute";
+  } else {
+    return rhours + " heures et " + rminutes + " minutes";
+  }
+}
+
+
+export function formatInput(input) {
+  const returnValue = input.toLowerCase()
+      .replace(/[àâä]/g, 'a')
+      .replace(/[æ]/g, 'ae')
+      .replace(/[ç]/g, 'c')
+      .replace(/[éèêë]/g, 'e')
+      .replace(/[îï]/g, 'i')
+      .replace(/[ô]/g, 'o')
+      .replace(/[œ]/g, 'oe')
+      .replace(/[ùûü]/g, 'u')
+      .replace(/\W/g, ' ')
+      .replace(/ +/g, '+')
+  return returnValue;
+  }
+
+
+export function get_code3_dep(x){
+    let strx = x.toString();
+    if (strx.length===3)
+        return x;
+    else
+        return "0" + x;
+}
+
+
+export function inside(point, vs) {
+
+  var x = point[0], y = point[1];
+
+  var inside = false;
+  for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+      var xi = vs[i][0], yi = vs[i][1];
+      var xj = vs[j][0], yj = vs[j][1];
+
+      var intersect = ((yi > y) != (yj > y))
+          && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+      if (intersect) inside = !inside;
+  }
+
+  return inside;
+};
