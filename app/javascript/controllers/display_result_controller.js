ba80131bdf42df64bcfc75e7c4e5a58d07e30172
Bastien Perez Assael <perez.bas@gmail.com>
2022-09-07 14:58:54 +0200

    counter

--- app/javascript/controllers/display_result_controller.js
+++ app/javascript/controllers/display_result_controller.js
@@ -5,7 +5,7 @@ import {timeConvert} from './utilities.js';
 export default class extends Controller {
   static targets = ["form", "caroussel", "comparated", "annualfp", "jetmn", "kmyacht", "tweet", "tweettext", "mailtext", "action"]
   connect() {
-    console.log("Test display 1")
+    console.log("DISPLAY CONNECTED 5")
   }
 
 
@@ -21,7 +21,10 @@ export default class extends Controller {
       .then((data) => {
         console.log(data)
         let result = data.match(/"resultvalue">([\d\.]+)/)
+        console.log("aaa")
+        console.log(result)
         result = result[1]
+        console.log("bbb")
         console.log(result)
         const annualFootprint = Math.round(Number.parseFloat(result))
         const jetComparaison = timeConvert((Number.parseInt(result)/78))
@@ -30,9 +33,9 @@ export default class extends Controller {
         this.carousselTarget.classList.toggle('d-none')
         this.comparatedTarget.classList.toggle('d-none')
         this.annualfpTarget.innerText = annualFootprint
-        this.actionTarget.innerHTML = "<strong>Envie d'agir ?</strong><br><a data-item-comp-target='tweet' href='' target='_blank'>Partagez votre résultat sur Twitter</a> ou recherchez votre député.e pour l'interpeller :"
+        this.actionTarget.innerHTML = "<strong>Envie d'agir ?</strong><br><a data-display-result-target='tweet' data-action='click->tweet-counter#increaseTweetCounter' href='' target='_blank'>Partagez votre résultat sur Twitter</a> ou recherchez votre député.e pour l'interpeller :"
         this.jetmnTarget.innerText = jetComparaison
-        this.kmyachtTarget.innerText = yachtComparaison
+        this.kmyachtTarget.innerText = `${yachtComparaison} kilomètres`
         this.tweetTarget.href = `https://twitter.com/intent/tweet?text=Mon empreinte CO2 annuelle est de ${annualFootprint} kgCO2e. Cela correspond à la consommation carburant de ${jetComparaison} de vol du jet privé de Vincent Bolloré, ou de ${yachtComparaison} kilomètres parcourus par le yacht de Bernard Arnault. Que font nos représentants ? &hashtags=quinousbrule`
         this.tweettextTarget.innerText = `Mon empreinte annuelle de ${annualFootprint} kgCO2e équivaut à ${jetComparaison} de vol du jet de Vincent Bolloré, ou ${yachtComparaison} kilomètres parcourus par le yacht de Bernard Arnault. Que faites-vous pour lutter contre les responsables du réchauffement climatique ?&hashtags=quinousbrule`
         this.mailtextTarget.innerText = `Le site quinousbrule.fr m'a permis de réaliser que mon empreinte carbone annuelle de ${annualFootprint} kgCO2e par an équivaut à la consommation carburant de ${jetComparaison} de vol du jet de Vincent Bolloré, ou de ${yachtComparaison} kilomètres parcourus par le yacht de Bernard Arnault.%0D%0A%0D%0AQue faites-vous pour lutter contre les vrais responsables du réchauffement climatique ?%0D%0A%0D%0ACordialement,%0D%0A%0D%0AVOTRE NOM`
